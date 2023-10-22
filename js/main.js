ba5838b47db67774d29e3c629bd4e035f4ab153f
jaseer <jassjaseerdazzils2000@gmail.com>
2023-10-22 20:59:26 +0530

    index compleated

--- js/main.js
+++ js/main.js
@@ -44,3 +44,20 @@
     
 })(jQuery);
 
+
+
+$(document).ready(function () {
+    // Smooth scrolling for all links with the hash (#) in the href attribute
+    $('a[href*="#"]').not('[href="#"]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000); // Adjust the animation speed here (in milliseconds)
+                return false;
+            }
+        }
+    });
+});
